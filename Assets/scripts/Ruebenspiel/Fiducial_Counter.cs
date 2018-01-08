f9d8e4e3d51a8e6c9332538f575ae53fab30a92e
Sinem Celik <sinemcelik@Sinems-MBP.fh-reutlingen.de>
2018-01-08 11:20:40 +0100

    Ruebenspiel hinzugefügt

---
+++ Assets/scripts/Ruebenspiel/Fiducial_Counter.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fiducial_Counter : MonoBehaviour {
+
+	public static int count = 1;
+	public int fiducialId = 0;
+	public static float time;
+
+	// Use this for initialization
+	void Start () {
+	
+		time = Time.time;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Time.time - time > 5){
+		bool done = true;
+		for(int i = 0; i < 4 ;i++){
+				done &= RuebenController.ScreenArray [i] >= 3f;	
+		}
+
+		if (done) {
+
+			time = Time.time;
+			// ziehe die Rübe aus der Mitte
+			GameObject carrot = GameObject.Find ("Ruebe" + count);
+			carrot.active = false; 
+			count++;
+
+			for (int i = 0; i < 4; i++) {
+					RuebenController.ScreenArray [i] = 0;
+			}
+		}
+	}
+	}
+}
---
+++ Assets/scripts/Ruebenspiel/LoadScreen.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadScreen : MonoBehaviour {
+
+	[SerializeField]
+	private float units;
+	float timeAMT = 10;
+	float time;
+	public Text timeText;
+
+//	public float sec = 10f;
+//	public float startVal = 0f;
+//	public float progress = 0f;
+
+	[SerializeField]
+	private Image fill;
+
+	private float fillAmount;
+
+	// Use this for initialization
+	void Start () {
+	//	Start (count);
+
+		time = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		fill.fillAmount = RuebenController.ScreenArray [0]/3;
+	}
+
+	/*
+
+	public IEnumerator BuildUnits(){
+
+		//Vector2 topLeft = Camera.main.ViewportToWorldPoint(new Vector2(0,1));
+		//Vector2 bottomRight = Camera.main.ViewportToWorldPoint (new Vector2 (1, 0));
+
+		for(int i = 0; 1 <= units; i++){
+
+
+			fillAmount = i / units;
+		//	Instantiate (count);
+			yield return null;
+		}
+		count++;
+	}
+
+	private void UpdateBar(){
+
+		fill.fillAmount = fillAmount;
+
+	} */
+}
