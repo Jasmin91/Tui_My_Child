8edeb7cd991b2ed19eba14d0a6ddeef6b647bcdd
Sinem Celik <sinemcelik@Sinems-MacBook-Pro.local>
2018-01-24 01:04:41 +0100

    Sound hinzufügen

---
+++ Assets/scripts/Ruebenspiel/SoundManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+
+
+	//Soundkomponenten
+	public AudioSource WinSound;
+//	public AudioSource PlingSound;
+
+
+	// Use this for initialization
+	void Start () {
+		WinSound = GetComponent<AudioSource>();
+	//	PlingSound = GameObject.Find ("Musik").GetComponent<AudioSource> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (Fiducial_Counter.play == true) {
+			WinSound.Play ();
+		}
+
+	/*	if (Fiducial_Counter.playPling == true) {
+			PlingSound.Play ();
+		} */
+
+	}
+}
